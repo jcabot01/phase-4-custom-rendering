081aee3b38a4214cc94c593d4ed467dc5059ba8b
JonCabot <jfc0053@gmail.com>
2022-05-23 20:36:35 -0500

    completed assignment

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,19 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    # render json: cheeses, only: [:id, :name]
+    # render json: cheeses, except: [:created_at, :updated_at]
+    render json: cheeses, except: [:created_at, :updated_at], methods: [:summary]  #able to call methods from our Models
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    if cheese
+      render json: cheese
+    else
+      render json: { error: "Cheese not found" }, status: :not_found  #use not_found or numeric status code like 404
+    end
   end
 
 end
